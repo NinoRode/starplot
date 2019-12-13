71c1715607fa88dfa55b86061551e42da6d3f162
NinoRode <nino.rode@gmail.com>
2019-12-13 16:29:42 +0100

    Shaping grid.nstar to be a drop-in replacement for grid.circle.

--- R/grid.nstar.R
+++ R/grid.nstar.R
@@ -2,70 +2,103 @@ library(grid)
 
 # phi <- (sqrt(5) + 1) / 2
 
-nstar <- function(vert = 5, rtio = NA, color = "blue", fill = "blue") {
+nstar <- function(vert = 5, ratio = NA, color = "blue", fill = "blue") {
 
   if (vert < 0) vert <- -vert
   pi_dvert <- pi / (2 * vert)
 
-  if (is.na(rtio)) {
+  if (is.na(ratio)) {
     # Izračunaj ramerje med polmeroma notranjega in zunanjega kroga za pravilno zvezdo
     # (vert - 4) * pi / (2 * vert) => pol vrha kraka zvezde
     # (vert + 2) * pi / (2 * vert) => "tretji" kot
-    rtio <- sin((vert - 4) * pi_dvert) / sin((vert + 2) * pi_dvert)
+    ratio <- sin((vert - 4) * pi_dvert) / sin((vert + 2) * pi_dvert)
     }
-  if (rtio > 1) rtio <- 1 / rtio
+  if (ratio > 1) ratio <- 1 / ratio
   b <- c(0:(2 * vert - 1)) * 2 * pi_dvert + pi/2
-  step <- c(rep(c(1, rtio), vert))
+  step <- c(rep(c(1, ratio), vert))
 
-  x <- (cos(b) * step)
-  y <- (sin(b) * step)
+  x <- ((cos(b) * step) + 1) / 2
+  y <- ((sin(b) * step) + 1) / 2
 
   # polygonGrob(x, y, default.units = "native", vp = vp)
   grid.polygon(x, y, default.units = "native", vp = vp)
 }
 
-# Point shapes:
-#
-# triangle, invtriangle, star3
-# square, diamond, diamond_slim,  star4
-# pentagon, star5, star5slim, star5fat, star5double,
-# hexagon, star6, star6slim, star6double
-# star8, star8double
+grid.nstar <- function(x=0.5, y=0.5, r=0.5, default.units="npc",
+                       name=NULL, gp=gpar(), draw=TRUE, vp=NULL,
+                       shape = "circle", point = 0, ratio = NA) {
+  # Posible shapes:
+  # triangle, invtriangle, star3
+  # square, diamond, diamond_slim,  star4
+  # pentagon, star5, star5slim, star5fat, star5double,
+  # hexagon, star6, star6slim, star6double
+  # star8, star8double,
+  # circle
+  switch(shape,
+         circle = grid.circle(x, y, r, default.units,
+                              name, gp, draw, vp),
+         triangle =  ,
+         invtriangle =  ,
+         star3 =  ,
+         square =  ,
+         diamond =  ,
+         diamond_slim =  ,
+         star4 =  ,
+         pentagon =  ,
+         star5 =  ,
+         star5slim =  ,
+         star5fat =  ,
+         star5double =  ,
+         hexagon =  ,
+         star6 =  ,
+         star6slim =  ,
+         star6double =  ,
+         star8 =  ,
+         star8double =  ,
+         if (point == 0) {
+           grid.circle(x, y, r, default.units,
+                   name, gp, draw, vp)
+         }
+         else {
+           nstar(point, ratio)
+         }
+  )
+}
 
 triangle <- function() {
   b <- c(0:2) * 2 * pi / 3 + pi/2
-  # polygonGrob((cos(b)), (sin(b)), default.units = "native", vp = vp)
-  grid.polygon((cos(b)), (sin(b)), default.units = "native", vp = vp)
+  # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
+  grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
 }
 
 invtriangle <- function() {
   b <- c(0:2) * 2 * pi / 3 + 3 * pi/2
-  # polygonGrob((cos(b)), (sin(b)), default.units = "native", vp = vp)
-  grid.polygon((cos(b)), (sin(b)), default.units = "native", vp = vp)
+  # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
+  grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
 }
 
 square <- function() {
   b <- c(0:3) * 2 * pi / 4 + pi/4
-  # polygonGrob((cos(b)), (sin(b)), default.units = "native", vp = vp)
-  grid.polygon((cos(b)), (sin(b)), default.units = "native", vp = vp)
+  # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
+  grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
 }
 
 diamond <- function() {
   b <- c(0:3) * 2 * pi / 4 + pi/2
-  # polygonGrob((cos(b)), (sin(b)), default.units = "native", vp = vp)
-  grid.polygon((cos(b)), (sin(b)), default.units = "native", vp = vp)
+  # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
+  grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
 }
 
 pentagon <- function() {
   b <- c(0:4) * 2 * pi / 5 + pi/2
-  # polygonGrob((cos(b)), (sin(b)), default.units = "native", vp = vp)
-  grid.polygon((cos(b)), (sin(b)), default.units = "native", vp = vp)
+  # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
+  grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
 }
 
 hexagon <- function() {
   b <- c(0:4) * 2 * pi / 5 + pi/2
-  # polygonGrob((cos(b)), (sin(b)), default.units = "native", vp = vp)
-  grid.polygon((cos(b)), (sin(b)), default.units = "native", vp = vp)
+  # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
+  grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
 }
 
 star3 <- function() {
@@ -116,7 +149,7 @@ star8double <- function() {
   nstar(8, -0.6)
 }
 
-vp <- viewport(xscale = c(-1, 1), yscale = c(-1, 1), name = "ta_view")
+vp <- viewport(name = "ta_view")
 pushViewport(vp)
 nstar(5, -1)
 nstar(6, sqrt(3))
