bc8a9c18d4ecf3caf62be3fe4e77c6fe5f9847dd
NinoRode <nino.rode@gmail.com>
2019-12-13 16:41:55 +0100

    Completed switch.

--- R/grid.nstar.R
+++ R/grid.nstar.R
@@ -37,24 +37,47 @@ grid.nstar <- function(x=0.5, y=0.5, r=0.5, default.units="npc",
   switch(shape,
          circle = grid.circle(x, y, r, default.units,
                               name, gp, draw, vp),
-         triangle =  ,
-         invtriangle =  ,
-         star3 =  ,
-         square =  ,
-         diamond =  ,
-         diamond_slim =  ,
-         star4 =  ,
-         pentagon =  ,
-         star5 =  ,
-         star5slim =  ,
-         star5fat =  ,
+         triangle =  {
+           b <- c(0:2) * 2 * pi / 3 + pi/2
+           grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
+         },
+         invtriangle = {
+           b <- c(0:2) * 2 * pi / 3 + 3 * pi/2
+           grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
+         },
+         star3 =  nstar(3, 0.5),
+         square =  {
+           b <- c(0:3) * 2 * pi / 4 + pi/4
+           # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
+           grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
+         },
+         diamond =  {
+           b <- c(0:3) * 2 * pi / 4 + pi/2
+           # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
+           grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
+         },
+         diamondslim =  nstar(2, 0.6),
+         star4 =  nstar(4, 0.5),
+         pentagon =  {
+           b <- c(0:4) * 2 * pi / 5 + pi/2
+           # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
+           grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
+         },
+         star5 = nstar(5),
+         star5slim = nstar(5, 0.3),
+         star5fat =  nstar(5, .6),
          star5double =  ,
-         hexagon =  ,
-         star6 =  ,
-         star6slim =  ,
-         star6double =  ,
-         star8 =  ,
-         star8double =  ,
+         hexagon =  {
+           b <- c(0:4) * 2 * pi / 5 + pi/2
+           # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
+           grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
+         },
+         star6 = nstar(6),
+         star6slim = nstar(6, 0.3),
+         star6double = nstar(6, -0.8),
+         star8 = nstar(8),
+         star8slim  = nstar(8, 0.5),
+         star8double = nstar(8, -0.8),
          if (point == 0) {
            grid.circle(x, y, r, default.units,
                    name, gp, draw, vp)
@@ -65,89 +88,9 @@ grid.nstar <- function(x=0.5, y=0.5, r=0.5, default.units="npc",
   )
 }
 
-triangle <- function() {
-  b <- c(0:2) * 2 * pi / 3 + pi/2
-  # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
-  grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
-}
-
-invtriangle <- function() {
-  b <- c(0:2) * 2 * pi / 3 + 3 * pi/2
-  # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
-  grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
-}
-
-square <- function() {
-  b <- c(0:3) * 2 * pi / 4 + pi/4
-  # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
-  grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
-}
-
-diamond <- function() {
-  b <- c(0:3) * 2 * pi / 4 + pi/2
-  # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
-  grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
-}
-
-pentagon <- function() {
-  b <- c(0:4) * 2 * pi / 5 + pi/2
-  # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
-  grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
-}
-
-hexagon <- function() {
-  b <- c(0:4) * 2 * pi / 5 + pi/2
-  # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
-  grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
-}
-
-star3 <- function() {
-  nstar(3, 0.5)
-}
-
-diamondslim <- function() {
-  nstar(2, 0.75)
-}
-
-star4 <- function() {
-  nstar(4, 0.5)
-}
-
-star5slim <- function() {
-  nstar(5, 0.3)
-}
-
-star5 <- function() {
-  nstar(5)
-}
-
-star5fat <- function() {
-  nstar(5, .6)
-}
-
-star5double <- function() {
-  nstar(5, -1.5)
-}
-
-star6 <- function() {
-  nstar(6)
-}
-
-star6slim <- function() {
-  nstar(6, 0.3)
-}
-
-star6double <- function() {
-  nstar(5, -1)
-}
-
-star8slim <- function() {
-  nstar(8, 0.5)
-}
-
-star8double <- function() {
-  nstar(8, -0.6)
-}
+# star5double <- function() { Treba najti način za neparne zvezde
+#   nstar(5, -1.5)
+# }
 
 vp <- viewport(name = "ta_view")
 pushViewport(vp)
