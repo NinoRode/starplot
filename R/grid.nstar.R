b1ca4e79797c86c0bb2676c422b09c75ed0c886f
NinoRode <nino.rode@gmail.com>
2019-12-15 12:11:34 +0100

    Code cleaning

--- R/grid.nstar.R
+++ R/grid.nstar.R
@@ -2,7 +2,7 @@ library(grid)
 
 # phi <- (sqrt(5) + 1) / 2
 
-nstar <- function(vert = 5, ratio = NA, color = "blue", fill = "blue") {
+nstar <- function(vert = 5, ratio = NA) {
 
   if (vert < 0) vert <- -vert
   pi_dvert <- pi / (2 * vert)
@@ -20,13 +20,13 @@ nstar <- function(vert = 5, ratio = NA, color = "blue", fill = "blue") {
   x <- ((cos(b) * step) + 1) / 2
   y <- ((sin(b) * step) + 1) / 2
 
-  # polygonGrob(x, y, default.units = "native", vp = vp)
-  grid.polygon(x, y, default.units = "native", vp = vp)
+  # polygonGrob(x, y, default.units = "native")
+  grid.polygon(x, y, default.units = "npc")
 }
 
 grid.nstar <- function(x=0.5, y=0.5, r=0.5, default.units="npc",
                        name=NULL, gp=gpar(), draw=TRUE, vp=NULL,
-                       shape = "circle", point = 0, ratio = NA) {
+                       shape = "", point = NA, ratio = NA) {
   # Posible shapes:
   # triangle, invtriangle, star3
   # square, diamond, diamond_slim,  star4
@@ -34,34 +34,35 @@ grid.nstar <- function(x=0.5, y=0.5, r=0.5, default.units="npc",
   # hexagon, star6, star6slim, star6double
   # star8, star8double,
   # circle
+  if (!is.na(point)) shape < NULL
   switch(shape,
          circle = grid.circle(x, y, r, default.units,
                               name, gp, draw, vp),
          triangle =  {
            b <- c(0:2) * 2 * pi / 3 + pi/2
-           grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
+           grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native")
          },
          invtriangle = {
            b <- c(0:2) * 2 * pi / 3 + 3 * pi/2
-           grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
+           grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native")
          },
          star3 =  nstar(3, 0.5),
          square =  {
            b <- c(0:3) * 2 * pi / 4 + pi/4
-           # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
-           grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
+           # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native")
+           grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native")
          },
          diamond =  {
            b <- c(0:3) * 2 * pi / 4 + pi/2
-           # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
-           grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
+           # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native")
+           grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native")
          },
          diamondslim =  nstar(2, 0.6),
          star4 =  nstar(4, 0.5),
          pentagon =  {
            b <- c(0:4) * 2 * pi / 5 + pi/2
-           # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
-           grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
+           # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native")
+           grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native")
          },
          star5 = nstar(5),
          star5slim = nstar(5, 0.3),
@@ -69,8 +70,8 @@ grid.nstar <- function(x=0.5, y=0.5, r=0.5, default.units="npc",
          star5double =  ,
          hexagon =  {
            b <- c(0:4) * 2 * pi / 5 + pi/2
-           # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
-           grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native", vp = vp)
+           # polygonGrob(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native")
+           grid.polygon(((cos(b) + 1) / 2), ((sin(b) + 1) / 2), default.units = "native")
          },
          star6 = nstar(6),
          star6slim = nstar(6, 0.3),
@@ -95,7 +96,9 @@ grid.nstar <- function(x=0.5, y=0.5, r=0.5, default.units="npc",
 vp <- viewport(name = "ta_view")
 pushViewport(vp)
 nstar(5, -1)
-nstar(6, sqrt(3))
+grid.nstar(point = 6, ratio = sqrt(3))
+grid.nstar(point = 3)
+grid.nstar(shape = "triangle")
 nstar(7, -1)
 
 
